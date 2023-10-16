dcf0d46703101f4237b8570fedeeb69d4a64748d
Wangke <a920114226@163.com>
2023-10-17 01:16:09 +0800

    阅读代码框架，补充注释

--- Emulation.py
+++ Emulation.py
@@ -6,6 +6,7 @@ import queue
 JOB_NUM = 100  # 发送请求的个数
 
 # 在opt-1.3B上的实验数据 单位: ms
+#大概表示了Figure 4的图像
 x = [1, 4, 16, 64, 256, 512, 1024]
 first_time = [5.88, 5.93, 6.57, 8.04, 23.8, 43.9, 98.5]
 next_time = [5.13, 5.11, 5.16, 5.22, 5.52, 5.72, 5.82]
@@ -19,23 +20,23 @@ p2 = np.poly1d(z2)
 
 class Request:  # 推理请求，理论上输出长度未知，但为仿真实验，需要事先确定
     def __init__(self, j_id, prompt_length, output_length):
-        self.j_id = j_id
-        self.prompt_length = int(prompt_length)
-        self.output_length = int(output_length)
-        self.first_iter_time = fit_first_iter_time(prompt_length)
-        self.next_iter_time  = fit_next_iter_time(prompt_length)
+        self.j_id = j_id # 推理请求的id，唯一的标识每一个请求
+        self.prompt_length = int(prompt_length) # 输入长度，int用于确保输入了整数
+        self.output_length = int(output_length) # 输出长度，int用于确保输入了整数
+        self.first_iter_time = fit_first_iter_time(prompt_length) # 第一次迭代的推理时间,基于上面拟合的函数模型得出
+        self.next_iter_time  = fit_next_iter_time(prompt_length) # 之后每次迭代的推理时间,同样基于上面拟合的函数模型得出
         self.iter_count = 0 # 请求执行了几次迭代，iter_count==output_length时完成整个推理   
         self.priority = -1  # 请求目前处于第几级队列
         
         self.create_time = time.time()  # 请求创建时间
         
-class RequestGenerator(threading.Thread):
+class RequestGenerator(threading.Thread): # 用户线程，继承自threading.Thread类
 
     def __init__(self, arrival_rate):
         super().__init__()
-        self.arrival_rate = arrival_rate  # arrival rate = 1s / job interval
+        self.arrival_rate = arrival_rate  # 请求发送速率，arrival rate = 1s / job interval
         
-    def run(self):
+    def run(self): # 重写run方法
         prompt_length_list = []
         output_length_list = []
         
@@ -47,7 +48,7 @@ class RequestGenerator(threading.Thread):
                 if count == 0:
                     count += 1
                     continue
-
+# 将数据集中每行的第一个元素作为输入长度，第二个元素作为输出长度
                 prompt_length_list.append(row[0])
                 output_length_list.append(row[1])
                 
@@ -61,25 +62,25 @@ class RequestGenerator(threading.Thread):
 
             j_id += 1
             
-            time.sleep(1 / self.arrival_rate)
+            time.sleep(1 / self.arrival_rate) # 按照arrival rate控制请求发送速率
 
 
 # Define class
 class SkipJoinMLFQScheduler:
 
     def __init__(self, first_quantum=6, quantum_rate=4, queue_num=4):
-        # super().__init__()
-        self.quantum_list = []
-        self.multi_level_priority_queue = []
+        # super().__init__() ？
+        self.quantum_list = [] # 每个队列的时间片大小序列
+        self.multi_level_priority_queue = [] # 用列表存储多级队列
         self.executed = 0  # 已经完成的请求数量
 
         # first quantum/Q1 is the min iteration time
         for i in range(queue_num):
-            self.quantum_list.append(quantum_rate ** i)
-            temp_q = queue.Queue(-1) 
-            self.multi_level_priority_queue.append(temp_q)
+            self.quantum_list.append(quantum_rate ** i) # 每个队列的时间片大小为前一个队列的时间片大小的quantum_rate倍
+            temp_q = queue.Queue(-1) # 创建queue_num个队列，表示不同优先级
+            self.multi_level_priority_queue.append(temp_q) # 将创建的队列放入多级队列列表中
             
-        self.ave_jct = []
+        self.ave_jct = [] # 存储每个请求的JCT
 
     def getNewRequest(self, request: Request):
         # Todo: 处理缓冲区中新到达的request，根据他们的输入长度放入多级队列中
